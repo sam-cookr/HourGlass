37ed918ea6a539527ffbd1fc14d1c6675fc32dc0
sam-cookr <S.R.Cook2@newcastle.ac.uk>
2025-06-16 21:43:08 +0100

    Initial Commit

---
+++ HourGlass/HourGlassApp.swift
@@ -0,0 +1,32 @@
+//
+//  HourGlassApp.swift
+//  HourGlass
+//
+//  Created by Sam Cook on 16/06/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HourGlassApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ HourGlassTests/HourGlassTests.swift
@@ -0,0 +1,17 @@
+//
+//  HourGlassTests.swift
+//  HourGlassTests
+//
+//  Created by Sam Cook on 16/06/2025.
+//
+
+import Testing
+@testable import HourGlass
+
+struct HourGlassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
